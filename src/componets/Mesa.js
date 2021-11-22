a32bc9b822d002e9c2086b0ab932daf3992e62ed
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-11-22 11:57:58 -0300

    Substituir todos os arquivos

---
+++ src/componets/Mesa.js
@@ -0,0 +1,13 @@
+function Mesa (){
+    const mesa = `
+    <section class="search">
+    <form class="form">
+        <i class="fa fa-search form__icon"></i>
+        <input type="search" class="form__input" placeholder="Search GitHub username..." required>
+        <input type="submit" class="form__button" value="Search">
+    </form>
+    </section>  
+    `;   
+    return mesa;
+}
+export default Mesa;
---
+++ src/componets/header.js
@@ -0,0 +1,8 @@
+function Header (){
+    const header = `
+    <h1>FoodBox</h1>
+    <h2>Monte seu box e receba em 10 minutos!</h2>    
+    `;   
+    return header;
+}
+export default Header;
