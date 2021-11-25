fa3f4275c139d6da69c7a9b7b926ed23a882a38e
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-11-24 22:23:19 -0300

    Delete src directory

--- src/componets/button.js
+++
@@ -1,8 +0,0 @@
-function Button (){
-    const button = `
-    <h2 class="button-pedir">Pedir</h2>
-     <span class="button-total">00.00</span>   
-    `;   
-    return button;
-}
-export default Button;
--- src/componets/header.js
+++
@@ -1,8 +0,0 @@
-function Header (){
-    const header = `
-    <h1>FoodBox</h1>
-    <h2>Monte seu box e receba em 10 minutos!</h2>    
-    `;   
-    return header;
-}
-export default Header;
